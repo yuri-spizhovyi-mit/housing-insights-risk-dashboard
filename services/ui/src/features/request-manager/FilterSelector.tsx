f3b953c2353266b24a7e66077b3eba33b5b03714
SpizhovyiMaxDev <144062499+SpizhovyiMaxDev@users.noreply.github.com>
2025-10-13 09:26:18 -0700

    Made responsive filter layout

--- services/ui/src/features/request-manager/FilterSelector.tsx
+++ services/ui/src/features/request-manager/FilterSelector.tsx
@@ -11,9 +11,10 @@ function FilterSelector({
 }: FilterSelectorProps) {
   return (
     <select
+      name={value}
       value={value}
       onChange={(e) => handleValueUpdate(e.target.value)}
-      className="bg-transparent focus:outline-none px-1 text-sm sm:text-sm"
+      className="bg-transparent focus:outline-none px-1 text-sm sm:text-sm w-full flex-2"
     >
       {data.map((val) => (
         <option key={val} value={val} className="bg-neutral-900">
