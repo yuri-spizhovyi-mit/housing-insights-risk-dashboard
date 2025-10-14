e953d6c0dc6fab10493cee90bdfee745c741974f
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-10-13 23:41:27 -0700

    feat(fapi): relative

--- services/fapi/models/model_predictions.py
+++ services/fapi/models/model_predictions.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, Integer, Date, Numeric, Text, TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
-from services.fapi.db import Base
+from fapi.db import Base
 import uuid
 
 
