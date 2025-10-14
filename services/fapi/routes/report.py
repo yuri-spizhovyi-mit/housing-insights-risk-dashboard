21d66a74aaf3b489623f7a24fd848783dfa3c6e1
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-10-13 23:38:28 -0700

    feat(fapi): add relative import

--- services/fapi/routes/report.py
+++ services/fapi/routes/report.py
@@ -14,10 +14,10 @@ from reportlab.platypus import (
 )
 import matplotlib.pyplot as plt
 
-from services.fapi.db import get_db
-from services.fapi.models.model_predictions import ModelPrediction
-from services.fapi.models.risk_predictions import RiskPrediction
-from services.fapi.models.anomaly_signals import AnomalySignal
+from fapi.db import get_db
+from fapi.models.model_predictions import ModelPrediction
+from fapi.models.risk_predictions import RiskPrediction
+from fapi.models.anomaly_signals import AnomalySignal
 
 router = APIRouter(prefix="/report", tags=["report"])
 
